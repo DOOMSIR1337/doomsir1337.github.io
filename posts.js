afd01ec02f3b87bfc87025c6066cc1b34e33b67d
DOOMSIR1337 <doomsir1337@outlook.com>
2026-05-10 00:14:12 +0530

    Add files via upload

---
+++ posts.js
@@ -0,0 +1,15 @@
+const posts = [
+      { 
+        id: 1, 
+        date: "2026.05.08", 
+        title: "Hello there!", 
+        content: `
+          <p>Welcome to my <span class="template-highlight">website</span>! <br><br>This is gonna be a sort of a blog like thing, where I post about my projects. Some posts will be the monthly artwork showcase, where I summarize what I made in the month, and others will most probably be individual artwork breakdowns where I somewhat explain the process behind the artwork. <br><br> I might also make posts about other stuff now and then, maybe compositing techniques that I use or maybe even games, but not sure...<br><br></p>
+          <div class="template-panel">
+            <p><strong>SO YEAH:</strong> I guess this is a bit of a an intro to what's gonna be done here!</p>
+            <div class="template-btn-row" style="margin-bottom:0; margin-top:15px;">
+                <a href="#" class="template-action-btn protocol-btn" style="border-color:var(--red); color:var(--red);">RETURN TO ROOT</a>
+            </div>
+        ` 
+      },
+    ];
